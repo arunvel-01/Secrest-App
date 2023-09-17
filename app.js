bfe81cda7f2b7e7bf8b11e57074d76b3d4f2c88b
Arun Vel <arunvelv9253@gmail.com>
2023-09-17 23:51:04 +0530

    Level 4 Hashing and Salting

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10; 
 
 const app = express();
 
@@ -26,8 +27,7 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-const user = new mongoose.model("User", userSchema)
-
+const User = mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
     res.render("home");
@@ -37,44 +37,59 @@ app.get("/login", function(req, res){
     res.render("login");
 });
 
-
 app.get("/register", function(req, res){
     res.render("register");
 });
 
 app.post("/register", function(req, res){
-  const newUser = new user({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-
-  newUser.save()
-    .then(function() {
-      res.render("secrets");
-    })
-    .catch(function(err) {
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    if(err) {
       console.log(err);
+      return res.status(500).send("Internal Server Error");
+    }
+
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
     });
+
+    newUser.save()
+      .then(function() {
+        res.render("secrets");
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+      });
+  });
 });
 
 app.post("/login", async function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   try {
-    const foundUser = await user.findOne({ email: username }).exec();
-    if (foundUser && foundUser.password === password) {
-      res.render("secrets");
+    const foundUser = await User.findOne({ email: username }).exec();
+    if (foundUser) {
+      bcrypt.compare(password, foundUser.password, function(err, result) {
+        if(err) {
+          console.log(err);
+          return res.status(500).send("Internal Server Error");
+        }
+        if (result === true) {
+          res.render("secrets");
+        } else {
+          res.render("login", { error: "Invalid username or password" });
+        }
+      });
     } else {
       res.render("login", { error: "Invalid username or password" });
     }
   } catch (error) {
     console.error(error);
+    res.status(500).send("Internal Server Error");
   }
 });
-
-
-
 
 app.listen(3000, function () {
   console.log("Server is started on port 3000");
